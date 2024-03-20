4243e5888a3327bb58b9c9f34c42ecd47bf011ac
yangdonggyu8923 <yangdonggyu8923@gmail.com>
2024-03-20 18:46:33 +0900

    323

--- src/main/java/com/james/api/User/User.java
+++ src/main/java/com/james/api/User/User.java
@@ -1,40 +1,4 @@
 package com.james.api.User;
 
-import jakarta.persistence.*;
-import lombok.*;
-
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Getter
-@ToString
-@Entity(name="user")
 public class User {
-    @Id
-    @Column(name = "id", nullable = false)
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    private String username;
-    private String password;
-    private String checkPassword;
-    private String name;
-    private String phone;
-    private Long addressId;
-    private String job;
-    private double height;
-    private double weight;
-    @Builder(builderMethodName = "builder")
-    public User(Long id, String username, String password, String checkPassword, String name, String phone, String job, double height, double weight) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-        this.checkPassword = checkPassword;
-        this.name = name;
-        this.phone = phone;
-        this.job = job;
-        this.height = height;
-        this.weight = weight;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
