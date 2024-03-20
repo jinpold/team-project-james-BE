4651547d472b63740e9da25388d3168f142db4a6
jinpold <jinpold@gmail.com>
2024-03-20 18:03:35 +0900

    240320 tables

--- src/main/java/com/james/api/product/Product.java
+++ src/main/java/com/james/api/product/Product.java
@@ -1,4 +1,15 @@
 package com.james.api.product;
 
+import jakarta.persistence.*;
+import lombok.*;
+@Entity(name="product")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@ToString
 public class Product {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 }
